4810d6b0b4ed7e0750bba5d18baf642ed7dbdf50
Jalad Shrimali <78215000+jalad-shrimali@users.noreply.github.com>
2024-01-19 07:28:22 +0000

    datatypes in JS practice

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -2,4 +2,28 @@ let name = 'javascript'
 let bool = false
 let num = 198
 
-console.table([name, bool, num])
+// console.table([name, bool, num])
+
+// Primitive datatype
+//7 types: string, boolean, number, null, undefined, symbol, bigint
+
+const score =100
+const deci_score= 100.3
+const isLoggedIn= true
+const othername = "codeSpace"
+const id = Symbol('123')
+const diff_id = Symbol('123')
+
+const heros = ["ironman", "captain america", "spiderman"]
+let myObject = {
+    name: "hitesh",
+    age: 22,
+    lr: "RP"
+}
+
+const myFunction = function(){
+    console.log("Hey"+myObject.lr+"!!")
+}
+myFunction();
+// console.log(id == diff_id)
+// console.table([score, deci_score, isLoggedIn, name, id])
